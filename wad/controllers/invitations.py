a4bd50d08b331421d1a0381a5bfc69147ea8f10d
Szymon Pyżalski <zefciu@gmail.com>
2010-05-13 00:25:46 +0200

    Some work done in invitations' controller

--- wad/controllers/invitations.py
+++ wad/controllers/invitations.py
@@ -2,17 +2,25 @@ import logging
 
 from pylons import request, response, session, tmpl_context as c
 from pylons.controllers.util import abort, redirect_to
+from pylons.decorators import validate
 
-from wad.lib.base import BaseController
+from wad.lib.base import BaseController, render
 from wad import model
 from  wad.model import meta
 import random as rnd
 
 from authkit.authorize.pylons_adaptors import authorize
 from authkit.permissions import ValidAuthKitUser
+import formencode as fe
 
 
 log = logging.getLogger(__name__)
+
+class CodeSchema(fe.Schema):
+    code = fe.validators.String(
+        min = 8, max = 8, notEmpty = True
+    )
+
 
 class InvitationsController(BaseController):
 
@@ -29,6 +37,15 @@ class InvitationsController(BaseController):
         return 'ok'
 
     def confirmation_form(self):
-        return self.render('/base.html')
-    
+        return render('/invitations/confirmation_form.html')
+
+    @validate(schema = CodeSchema(), form='confirmation_form')
+    def submit_code(self):
+       invitation = meta.Session.query(model.Invitation).filter(
+           model.Invitation.code == self.form_result['code']
+       ).all();
+       if not len(invitation):
+           return 'Nie ma takiego zaproszenia'
+       else:
+           return 'Zaproszenie znalezione'
 
--- wad/lib/helpers.py
+++ wad/lib/helpers.py
@@ -3,5 +3,5 @@
 Consists of functions to typically be used within templates, but also
 available to Controllers. This module is available to templates as 'h'.
 """
-# Import helpers as desired, or define your own, ie:
-#from webhelpers.html.tags import checkbox, password
+from webhelpers.html.tags import form, text, submit, end_form
+from routes import url_for
