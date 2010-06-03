4d58c2b2d2e36d440b220d58328ce18409176841
Szymon Pyżalski <zefciu@gmail.com>
2010-06-03 17:51:52 +0200

    Some ugly styles

--- wad/lib/helpers.py
+++ wad/lib/helpers.py
@@ -4,5 +4,5 @@ Consists of functions to typically be used within templates, but also
 available to Controllers. This module is available to templates as 'h'.
 """
 from webhelpers.html.tags import form, text, submit, end_form, checkbox 
-from webhelpers.html.tags import javascript_link, stylesheet_link
+from webhelpers.html.tags import javascript_link, stylesheet_link, link_to
 from routes import url_for
